1ac6f9894488e4c3011459d19587d29e37404033
Tanyi Győző <tanyi.gyozo@gmail.com>
2023-06-16 18:17:25 +0200

    FileLogger and ScreenLogger implemented

---
+++ tesztpizza/Logger.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tesztpizza
+{
+    public abstract class Logger
+    {
+        protected StreamWriter writer;
+
+        public bool AutoClose { get; set; }
+
+        public Logger(bool autoClose)
+        {
+            AutoClose = autoClose;
+        }
+
+        public void Log(string message)
+        {
+            writer.WriteLine(message);
+            if (AutoClose)
+            {
+                Close();
+            }
+        }
+
+        public void Close()
+        {
+            writer?.Close();
+        }
+    }
+}
---
+++ tesztpizza/ScreenLogger.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tesztpizza
+{
+    public class ScreenLogger : Logger
+    {
+        public ScreenLogger(): base(false)
+        {
+            writer = new StreamWriter(Console.OpenStandardOutput());
+            writer.AutoFlush = true;
+        }
+    }
+}
